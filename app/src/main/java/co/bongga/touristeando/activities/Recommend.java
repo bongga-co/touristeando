8e863e9a99246060a1436abe2f8865161682945d
Silvio Palacios <info@bongga.co>
2017-03-22 23:04:29 -0500

    - Recommend a place

--- app/src/main/java/co/bongga/touristeando/activities/Recommend.java
+++ app/src/main/java/co/bongga/touristeando/activities/Recommend.java
@@ -2,13 +2,64 @@ package co.bongga.touristeando.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import co.bongga.touristeando.R;
+import co.bongga.touristeando.models.PlaceCategory;
 
 public class Recommend extends AppCompatActivity {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    private List<String> categoryList = new ArrayList<>();
+
+    private Spinner spCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        spCategory = (Spinner) findViewById(R.id.rc_place_category);
+
+        getCategories();
+    }
+
+    private void getCategories(){
+        DatabaseReference categoriesRef = databaseReference.child("place_categories");
+        categoriesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                categoryList.clear();
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    PlaceCategory category = item.getValue(PlaceCategory.class);
+                    categoryList.add(category.getName());
+                }
+
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_spinner_item, categoryList);
+
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spCategory.setAdapter(dataAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/co/bongga/touristeando/models/PlaceCategory.java
@@ -0,0 +1,24 @@
+package co.bongga.touristeando.models;
+
+/**
+ * Created by bongga on 3/22/17.
+ */
+
+public class PlaceCategory {
+    private String name;
+
+    public PlaceCategory() {
+    }
+
+    public PlaceCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
